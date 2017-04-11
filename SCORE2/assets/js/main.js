ee10b7350a6e1d35a0a9f5c6f9d48abf38e1cf6d
Jacob Varner <jacob.varner14@gmail.com>
2017-04-11 13:27:39 -0500

    Added mobile navigation and scaled the header for mobile.

--- SCORE2/assets/js/main.js
+++ SCORE2/assets/js/main.js
@@ -8,4 +8,14 @@ jQuery(document).ready(function ($) {
       $("#header-logo").removeClass("shrink-logo");
   	}
   });
+
+  $("#menu-icon").click(function () {
+    if ($("#mobile-navigation").css("display") === "none") {
+      $("#mobile-navigation").css("display", "block");
+      $("#menu-icon").attr("src", "assets/images/menu-icon-close.png");
+    } else {
+      $("#mobile-navigation").css("display", "none");
+      $("#menu-icon").attr("src", "assets/images/menu-icon.png");
+    }
+  });
 });
