ed7056e5c2123b2a3820533d91d5a363b490c107
Jacob Varner <jacob.varner14@gmail.com>
2017-04-11 11:59:28 -0500

    Got shrinking header working.

--- SCORE2/assets/js/main.js
+++ SCORE2/assets/js/main.js
@@ -1,13 +1,11 @@
-window.onscroll = function() {navScroll()};
-
-function navScroll() {
-  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-    document.getElementById("navigation").style.position = "fixed";
-    document.getElementById("header").style.height = "60px"
-    document.getElementById("header").style.position = "fixed";
-    document.getElementById("header-logo").style.display = "none";
-    document.getElementById("header-logo-small").style.display = "block";
-  } else {
-    document.getElementById("myBtn").style.display = "none";
-  }
-}
+jQuery(document).ready(function ($) {
+  $(document).on("scroll", function() {
+  	if ($(document).scrollTop() > 100) {
+  	  $("header").addClass("shrink");
+      $("#header-logo").addClass("shrink-logo");
+  	} else {
+  	  $("header").removeClass("shrink");
+      $("#header-logo").removeClass("shrink-logo");
+  	}
+  });
+});
